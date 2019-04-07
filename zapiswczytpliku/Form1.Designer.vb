02e05fe1a8a72034a593c05f6703b782bd617a59
konradszwed <konradszwed11@gmail.com>
2019-04-07 11:47:50 +0200

    Edytor tekstu VB

---
+++ zapiswczytpliku/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button5 = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(292, 260)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 293)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(153, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Pobierz tekst z pliku"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(13, 428)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(185, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Zapisz tekst do pliku"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(185, 296)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 371)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(16, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "..."
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(311, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(488, 409)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(311, 428)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(141, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Wczytaj obraz"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(493, 427)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(143, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Wczytaj wiele obrazow"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(695, 427)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "Slajdy"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(811, 498)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
---
+++ zapiswczytpliku/Form1.vb
@@ -0,0 +1,166 @@
+﻿Public Class Form1
+
+    Dim i As Integer
+    REM tablica 5 sciezki
+    Dim sciezka(4) As String
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nazwapliku As String
+        Dim liczba_l As Integer
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.InitialDirectory = "c:\"
+        openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        openFileDialog1.FilterIndex = 2
+        openFileDialog1.RestoreDirectory = True
+        If openFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            nazwapliku = openFileDialog1.FileName()
+            MsgBox(nazwapliku)
+            RichTextBox1.LoadFile(nazwapliku, RichTextBoxStreamType.PlainText)
+        End If
+        liczba_l = RichTextBox1.Lines.Count()
+        MsgBox(liczba_l)
+        NumericUpDown1.Maximum = liczba_l - 1
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        Dim n As Integer
+        n = NumericUpDown1.Value
+        Label1.Text = RichTextBox1.Lines(n)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim nazwapliku As String
+        Dim saveFileDialog1 As New SaveFileDialog()
+        saveFileDialog1.InitialDirectory = "c:\"
+        saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        saveFileDialog1.FilterIndex = 1
+        saveFileDialog1.RestoreDirectory = True
+        If saveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            nazwapliku = saveFileDialog1.FileName()
+            MsgBox(nazwapliku)
+            RichTextBox1.SaveFile(nazwapliku, RichTextBoxStreamType.PlainText)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim nazwapliku As String
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.InitialDirectory = "c:\"
+        openFileDialog1.Filter = "png files (*.png)|*.png|All files (*.*)|*.*"
+        openFileDialog1.FilterIndex = 2
+        openFileDialog1.RestoreDirectory = True
+        If openFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            nazwapliku = openFileDialog1.FileName()
+            MsgBox(nazwapliku)
+            PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+            PictureBox1.Load(nazwapliku)
+        End If
+    End Sub
+
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Select Case i
+            Case 0
+                PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+            Case 1
+                PictureBox1.SizeMode = PictureBoxSizeMode.CenterImage
+            Case 2
+                PictureBox1.SizeMode = PictureBoxSizeMode.Normal
+            Case 3
+                PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+            Case 4
+                PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        End Select
+        i = i + 1
+        If i = 5 Then i = 0
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 1000
+        REM Timer1.Start() 
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Select i
+            Case 0
+
+                PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+
+            Case 1
+
+                PictureBox1.SizeMode = PictureBoxSizeMode.CenterImage
+
+            Case 2
+
+                PictureBox1.SizeMode = PictureBoxSizeMode.Normal
+
+            Case 3
+
+                PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+
+            Case 4
+
+                PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+
+        End Select
+        i = i + 1
+        If i = 5 Then i = 0
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Select Case i
+            Case 0
+                PictureBox1.Load(sciezka(0))
+            Case 1
+                PictureBox1.Load(sciezka(1))
+
+            Case 2
+                PictureBox1.Load(sciezka(2))
+
+            Case 3
+                PictureBox1.Load(sciezka(3))
+
+            Case 4
+                PictureBox1.Load(sciezka(4))
+
+        End Select
+        i = i + 1
+        If i = 5 Then i = 0
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        For i As Integer = 0 To 4
+
+            Dim nazwapliku As String
+            Dim openFileDialog1 As New OpenFileDialog()
+            openFileDialog1.InitialDirectory = "c:\"
+            openFileDialog1.Filter = "png files (*.png)|*.png|All files (*.*)|*.*"
+            openFileDialog1.FilterIndex = 2
+            openFileDialog1.RestoreDirectory = True
+            If openFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                nazwapliku = openFileDialog1.FileName()
+                MsgBox(nazwapliku)
+                PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+                PictureBox1.Load(nazwapliku)
+                sciezka(i) = openFileDialog1.FileName()
+
+
+            End If
+        Next
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If Timer1.Enabled = True Then
+            Timer1.Enabled = False
+        Else
+            Timer1.Enabled = True
+        End If
+
+
+    End Sub
+End Class
